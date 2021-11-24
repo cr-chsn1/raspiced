90275543a0f19fe16bc43f060360a225144e7278
cr_chsn1 <6362230+cr-chsn1@users.noreply.github.com>
2021-11-24 17:58:56 +0100

    Added more system information
    Version 1.3

--- hwbot_prime_benchmark/script/rpi_hwbot.sh
+++ hwbot_prime_benchmark/script/rpi_hwbot.sh
@@ -18,7 +18,7 @@ printf "${color_cyan}#########################################${color_reset}\n"
 printf "${color_cyan}#                                       #${color_reset}\n"
 printf "${color_cyan}#  HWBOT Prime Script for Raspberry Pi  #${color_reset}\n"
 printf "${color_cyan}#                                       #${color_reset}\n"
-printf "${color_cyan}# by cr_chsn1               Version 1.2 #${color_reset}\n"
+printf "${color_cyan}# by cr_chsn1               Version 1.3 #${color_reset}\n"
 printf "${color_cyan}#########################################${color_reset}\n"
 echo
 echo
@@ -36,10 +36,13 @@ fi
 printf "${color_green}### Software-Information ###${color_reset}\n"
 echo
 printf "${color_green}Raspbian-Version:${color_reset}\n"
-cat /etc/debian_version 
+os_name=$(lsb_release -si)
+os_codename=$(lsb_release -sc)
+os_version=$(cat /etc/debian_version)
+echo $os_name $os_version "("$os_codename")"
 echo
 printf "${color_green}Linux-Kernel:${color_reset}\n"
-uname -a
+uname -r -v
 echo
 printf "${color_green}Java Runtime:${color_reset}\n"
 java -version
@@ -53,7 +56,7 @@ echo
 printf "${color_yellow}Identification (/proc/device-tree/compatible):${color_reset}\n"
 cat /proc/device-tree/compatible && echo
 echo
-printf "${color_yellow}Sensor Status:${color_reset}\n"
+printf "${color_yellow}Sensor Status (1/2):${color_reset}\n"
 freq_arm_org=$(vcgencmd measure_clock arm)
 freq_arm=${freq_arm_org:14:10}
 freq_arm=$(convert_to_MHz $freq_arm)
@@ -61,15 +64,22 @@ volt_arm=$(vcgencmd measure_volts core)
 volt_arm=${volt_arm:5:4}
 volt_mem=$(vcgencmd measure_volts sdram_c)
 volt_mem=${volt_mem:5:4}
-temp=$(vcgencmd measure_temp)
-temp=${temp:5:4}
+temp_idle=$(vcgencmd measure_temp)
+temp_idle=${temp_idle:5:4}
 echo "Frequency (arm):	$freq_arm MHz ($freq_arm_org)"
 echo "Voltage (arm):		$volt_arm V"
 echo "Voltage (sdram):	$volt_mem V"
-echo "Temperature (idle):	$temp °C"
+echo "Temperature (idle):	$temp_idle °C"
 echo
 echo
 printf "${color_red}### Benchmark ###${color_reset}\n"
 echo
 date_time=`date '+%Y-%m-%d_%H.%M.%S'`
-java -jar ./hwbotprime.jar --output=$date_time.hwbot
+java -jar ./hwbotprime.jar "$date_time $freq_arm MHz".hwbot
+echo
+echo
+printf "${color_yellow}Sensor Status (2/2):${color_reset}\n"
+temp_load=$(vcgencmd measure_temp)
+temp_load=${temp_load:5:4}
+echo "Temperature (post-load):	$temp_load °C"
+echo
